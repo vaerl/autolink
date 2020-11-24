141e720beceb6484ad33f8aa05050397495772d3
vaerl <lukas@mlel.dev>
2020-11-24 22:07:06 +0100

    Restore lost code.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use anyhow::{Context, Result};
+use log::{debug, info, warn};
+use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
 use structopt::StructOpt;
@@ -10,51 +12,83 @@ struct Arg {
     path: PathBuf,
 }
 
+// TODO eliminate unwrap if possible
+// TODO check return-types
 fn main() -> Result<()> {
-    // TODO add logging
+    env_logger::init();
+    info!("Starting.");
 
-    // read arguments into arg - is the struct necessary?
     let arg = Arg::from_args();
-    if arg.path.is_file() {
-        link(arg.path)
-    } else if arg.path.is_dir() {
-        // TODO get all files, iterate over
-        link(arg.path)
+    link(arg.path)
+}
+
+fn link(origin: PathBuf) -> Result<()> {
+    if origin.is_file() {
+        info!("Path {} is file, trying to symlink.", origin.display());
+        link_file(origin)
+    } else if origin.is_dir() {
+        info!(
+            "Path {} is directory, trying to symlink contents recursively.",
+            origin.display()
+        );
+        let paths = fs::read_dir(origin).unwrap();
+        for path in paths {
+            link(path.unwrap().path());
+        }
+        return Ok(());
     } else {
         Ok(())
     }
 }
 
-fn link(origin: PathBuf) -> Result<()> {
+fn link_file(origin: PathBuf) -> Result<()> {
     // open file from path
-    let content: String = std::fs::read_to_string(&origin).with_context(|| {
-        // format!(
-        //     "Could not read file `{}`",
-        //     &arg.path.into_os_string().into_string().unwrap() // this works - but is it nice?
-        // )
-        format!("Could not read file")
-    })?;
+    let content: String = std::fs::read_to_string(&origin)
+        .with_context(|| format!("Could not read file `{}`", &origin.display()))?;
 
     for line in content.lines() {
         if line.contains("##!!") {
             // TODO optimize -> use bufReader?
-            println!("{}", line);
+            debug!("Recognized tag: {}", line);
 
             // cut indicator from line, convert to path
             let substr: String = line.chars().into_iter().skip(4).collect();
-            // TODO check what happens when the substr is invalid!
             let destination: PathBuf = PathBuf::from(substr);
-            // TODO check if symlink exists
+            if !destination.exists() {
+                warn!(
+                    "Destination {} does not exist, skipping.",
+                    destination.display()
+                );
+                continue;
+            }
+
+            let mut full_origin: PathBuf = PathBuf::from(&destination);
+            full_origin.push(origin.file_name().unwrap());
+            if link_exists(full_origin) {
+                warn!("Link exists at {}, skipping.", destination.display());
+                continue;
+            }
+
             // symlink from the given path to destination
             // TODO convert to complete paths
-            // TODO is the & needed at origin?
             Command::new("ln")
                 .arg("-s")
-                .arg(&origin)
-                .arg(destination)
+                .arg(&origin.canonicalize().unwrap())
+                .arg(destination.canonicalize().unwrap())
                 .spawn()
                 .expect("Could not symlink.");
         }
     }
     Ok(())
+}
+
+fn link_exists(path: PathBuf) -> bool {
+    info!("Testing if symlink {} exists.", path.display());
+    match Command::new("test").arg("-f").arg(&path).output() {
+        Ok(output) => output.status.success(),
+        Err(err) => {
+            debug!("Something went wrong, returning false: {}", err);
+            false
+        }
+    }
 }
