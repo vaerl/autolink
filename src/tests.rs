18b39c91b9c4cd4fc1586cbc3e40892e0cd1bbb6
vaerl <lukas@mlel.dev>
2021-05-14 20:43:41 +0200

    Add some tests.

--- src/tests.rs
+++ src/tests.rs
@@ -1,19 +1,121 @@
-use super::*;
+// TODO write
+// TODO include origin: (), destinations: (), autolink: () origin: (), destinations: (), autolink: () during build
 
 #[cfg(test)]
 mod tests {
+
+    use path_absolutize::Absolutize;
+
+    use crate::{linkfile::LinkFile, Autolink};
+    use std::path::PathBuf;
+
+    // FINDING LINKS
+
     #[test]
-    fn link_file_bad_origin() {
-        // IMPLEMENT
+    fn get_links_from_file() {
+        let autolink = Autolink {
+            path: PathBuf::from("examples/file1.example"),
+            create_dirs: false,
+            overwrite: false,
+            delete: false,
+            verbose: false,
+        };
+
+        let link_file = autolink
+            .get_links(&PathBuf::from("examples/file1.example"))
+            .unwrap();
+        let expected_destinations = vec![
+            PathBuf::from(shellexpand::tilde("~/file1.example").to_string()),
+            PathBuf::from(shellexpand::tilde("~/dev/file1.example").to_string()),
+            // NOTE I have to pass examples as additional context for the file its being read from
+            PathBuf::from(shellexpand::tilde("examples/test/file1.example").to_string())
+                .absolutize()
+                .unwrap()
+                .into_owned(),
+        ];
+        assert_eq!(link_file.destinations, expected_destinations);
     }
 
     #[test]
-    fn link_file_exists() {
-        // IMPLEMENT
+    fn get_linkfiles_from() {
+        let autolink = Autolink {
+            path: PathBuf::from("examples/nested-dir"),
+            create_dirs: false,
+            overwrite: false,
+            delete: false,
+            verbose: false,
+        };
+
+        let linkfiles = autolink
+            .find_links(&PathBuf::from("examples/nested-dir"))
+            .unwrap();
+        let expected_linkfiles = vec![LinkFile {
+            origin: PathBuf::from("examples/nested-dir/file2.example")
+                .absolutize()
+                .unwrap()
+                .into_owned(),
+            autolink: &autolink,
+            destinations: vec![PathBuf::from("examples/test/nested-dir/file2.example")
+                .absolutize()
+                .unwrap()
+                .into_owned()],
+        }];
+
+        // iterate through both vecs at once
+        for (actual, expected) in linkfiles.iter().zip(expected_linkfiles) {
+            assert_eq!(actual.origin, expected.origin);
+            assert_eq!(actual.destinations, expected.destinations);
+        }
+    }
+
+    // LINKS
+
+    #[test]
+    fn create_link() {
+        let autolink = Autolink {
+            path: PathBuf::from("examples/file1.example"),
+            create_dirs: false,
+            overwrite: false,
+            delete: false,
+            verbose: false,
+        };
+
+        let linkfile = LinkFile {
+            origin: PathBuf::from("examples/file1.example")
+                .absolutize()
+                .unwrap()
+                .into_owned(),
+            destinations: vec![PathBuf::from("examples/nested-dir/file1.example")],
+            autolink: &autolink,
+        };
+
+        linkfile.link(false, false).unwrap();
+        assert!(PathBuf::from("examples/nested-dir/file1.example").is_file());
     }
 
     #[test]
-    fn link_file() {
-        // IMPLEMENT
+    fn delete_link() {
+        // create the link to not depend on any order of tests
+        create_link();
+
+        let autolink = Autolink {
+            path: PathBuf::from("examples/file1.example"),
+            create_dirs: false,
+            overwrite: false,
+            delete: false,
+            verbose: false,
+        };
+
+        let linkfile = LinkFile {
+            origin: PathBuf::from("examples/file1.example")
+                .absolutize()
+                .unwrap()
+                .into_owned(),
+            destinations: vec![PathBuf::from("examples/nested-dir/file1.example")],
+            autolink: &autolink,
+        };
+
+        linkfile.delete().unwrap();
+        assert!(!PathBuf::from("examples/nested-dir/file1.example").is_file());
     }
 }
